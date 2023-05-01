ba3eed1ab5f0cbdda0bdbbcd3e1d7704e04cd8fe
CodeAkio <siqueira_2004@hotmail.com>
2023-04-30 23:38:17 -0300

    feat: add jwt login

--- api/handlers.js
+++ api/handlers.js
@@ -2,6 +2,7 @@
 
 const { MongoClient, ObjectId } = require('mongodb');
 const { pbkdf2Sync } = require('crypto');
+const { sign, verify } = require('jsonwebtoken');
 
 let connectionInstance = null;
 async function connectToDatabase() {
@@ -14,44 +15,32 @@ async function connectToDatabase() {
   return connectionInstance;
 }
 
-async function basicAuth(event) {
-  const { authorization } = event.headers
+async function authorize(event) {
+  const { authorization } = event.headers;
   if (!authorization) {
     return {
       statusCode: 401,
       body: JSON.stringfy({ error: 'Missing authorization header' })
-    }
+    };
   }
 
-  const [type, credentials] = authorization.split(' ')
-  if (type != 'Basic') {
+  const [type, token] = authorization.split(' ');
+  if (type != 'Bearer' || !token) {
     return {
       statusCode: 401,
       body: JSON.stringfy({ error: 'Unsuported authorization type' })
-    }
+    };
   }
 
-  const [username, password] = Buffer.from(credentials, 'base64').toString().split(':');
-  const hashedPass = pbkdf2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex');
-
-  const client = await connectToDatabase()
-  const collection = await client.collection('users')
-  const user = await collection.findOne({
-    name: username,
-    password: hashedPass
-  });
-
-  if (!user) {
+  const decodedToken = verify(token, process.env.JWT_SECRET, { audience: 'alura-serverless' })
+  if (!decodedToken) {
     return {
       statusCode: 401,
-      body: JSON.stringfy({ error: 'Invalid Credentials' })
-    }
+      body: JSON.stringify({ error: 'Invalid token' })
+    };
   }
 
-  return {
-    id: user._id,
-    username: user.username
-  }
+  return decodedToken;
 }
 
 function extractBody(event) {
@@ -68,8 +57,40 @@ function extractBody(event) {
   return JSON.parse(event.body);
 }
 
+module.exports.login = async (event) => {
+  const { username, password } = extractBody(event)
+  const hashedPass = pbkdf2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex')
+
+  const client = await connectToDatabase()
+  const collection = await client.collection('users')
+  const user = await collection.findOne({
+    name: username,
+    password: hashedPass
+  })
+
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringfy({ error: 'Invalid Credentials' })
+    }
+  }
+
+  const token = sign({ username, id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '24h',
+    audience: 'alura-serverless'
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ token })
+  };
+}
+
 module.exports.sendResponse = async (event) => {
-  const authResult = await basicAuth(event);
+  const authResult = await authorize(event);
   if (authResult.statusCode == 401) return authResult;
 
   const { name, answers } = extractBody(event);
@@ -109,7 +130,7 @@ module.exports.sendResponse = async (event) => {
 }
 
 module.exports.getResult = async (event) => {
-  const authResult = await basicAuth(event);
+  const authResult = await authorize(event);
   if (authResult.statusCode == 401) return authResult;
 
   const client = await connectToDatabase();
